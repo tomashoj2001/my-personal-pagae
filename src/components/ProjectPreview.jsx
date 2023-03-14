729c408beb17de17cdafc1160feecbb10bf49e97
tomashoj2001 <tomashojnadel@gmail.com>
2023-03-13 21:42:44 -0300

    .

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -2,8 +2,8 @@ export default function ProjectPreview({ content, setDesktop }) {
   return (
   <section className="project__preview">
     <a className="project__slider" href={content.site} target="_blank">      
-      <img className="project__img project__img--desktop" alt="Proyecto"/>
-      <img className="project__img project__img--mobile" alt="Proyecto"/>
+      <img className="project__img project__img--desktop" src="/src/Assets/spacetourism-desktop.jpg" alt="Proyecto escritorio"/>
+      <img className="project__img project__img--mobile" src="/src/Assets/spacetourism-mobile.jpg" alt="Proyecto teléfono"/>
     </a>
     <div className="preview__buttons">
       <button onClick={() => setDesktop(0)}>Desktop</button>
