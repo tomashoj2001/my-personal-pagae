f1cda22ed0de547b351d1b98763adf975daf858f
tomashoj2001 <tomashojnadel@gmail.com>
2023-03-14 09:35:29 -0300

    .

--- src/components/DarkButton.jsx
+++ src/components/DarkButton.jsx
@@ -9,6 +9,7 @@ export default function DarkButton({ mode, setMode }) {
   }, [mode])
   
   let root = document.querySelector(':root')
+  let html = document.querySelector('html')
   if (mode === 'light') {
     root.style.setProperty('--first-color', 'rgb(168, 218, 220)')  
     root.style.setProperty('--second-color', 'rgb(29, 93, 133)')  
@@ -16,7 +17,7 @@ export default function DarkButton({ mode, setMode }) {
     root.style.setProperty('--fourth-color', 'rgb(194, 3, 3)')  
     root.style.setProperty('--fifth-color', 'rgb(29, 53, 87)')
     root.style.setProperty('--shadow-color', 'rgba(0, 0, 0, .35)')
-    root.style.setProperty('--bg-image', 'url(/src/Assets/background-pattern.png)')
+    html.style.backgroundImage = 'var(--light-bg)'
   } else {
     root.style.setProperty('--first-color', 'rgb(0, 35, 55)')  
     root.style.setProperty('--second-color', 'rgb(0, 153, 195)')  
@@ -24,7 +25,7 @@ export default function DarkButton({ mode, setMode }) {
     root.style.setProperty('--fourth-color', 'rgb(240, 149, 96)')  
     root.style.setProperty('--fifth-color', 'rgb(202, 240, 248)')
     root.style.setProperty('--shadow-color', 'rgba(200, 200, 200, .12)')
-    root.style.setProperty('--bg-image', 'url(/src/Assets/background-pattern-dark.png)')
+    html.style.backgroundImage = 'var(--dark-bg)'
   }
 
   return(
