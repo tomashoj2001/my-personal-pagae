3c0512f89a036b5611c5e71eeae5eaabffdeba97
TomiHojnadel <tomashojnadel@gmail.com>
2023-08-28 09:08:07 -0300

    updating projects

--- src/components/Project/ProjectNavbar.jsx
+++ src/components/Project/ProjectNavbar.jsx
@@ -2,11 +2,9 @@ export default function ProjectNavbar({ setProject }) {
   return (
     <nav className="project-navbar">
       <button onClick={() => setProject(0)}>Landing Page de inmueble</button>
-      <button onClick={() => setProject(1)}>
-        Multipage de Turismo Espacial
-      </button>
+      <button onClick={() => setProject(1)}>Coffy</button>
       <button onClick={() => setProject(2)}>Wiki de países</button>
-      <button onClick={() => setProject(3)}>Calculadora de Propinas</button>
+      <button onClick={() => setProject(3)}>Multipage de Turismo Espacial</button>
     </nav>
   );
 }
